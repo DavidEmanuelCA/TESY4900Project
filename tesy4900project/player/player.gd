8892b4d57683a124072a60782be8c786e8fab883
David <DEmanuelCAnaya@gmail.com>
2025-05-31 12:59:13 -0600

    added input map and better player movement

--- tesy4900project/player/player.gd
+++ tesy4900project/player/player.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 const GRAVITY = 1000
+const SPEED = 300
 
 enum STATE { IDLE, RUN }
 
@@ -12,8 +15,11 @@ func _ready():
 func _physics_process(delta):
 	player_falling(delta)
 	player_idle(delta)
+	player_run(delta)
 	
 	move_and_slide()
+	
+	player_animations()
 
 func player_falling(delta):
 	if !is_on_floor():
@@ -22,3 +28,21 @@ func player_falling(delta):
 func player_idle(delta):
 	if is_on_floor():
 		current_state = STATE.IDLE
+
+func player_run(delta):
+	var direction = Input.get_axis("move_left", "move_right")
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+	if direction != 0:
+		current_state = STATE.RUN
+		animated_sprite_2d.flip_h = false if direction > 0 else true
+
+func player_animations():
+	if current_state == STATE.IDLE:
+		animated_sprite_2d.play("idle")
+	elif current_state == STATE.RUN:
+		animated_sprite_2d.play("run")
