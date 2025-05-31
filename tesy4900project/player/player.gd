96c5897f57c81efa5d39a431c82791038b14a89a
David <DEmanuelCAnaya@gmail.com>
2025-05-31 14:11:45 -0600

    added player jump functionality and animation, but having to fix issue where direction cannot be altered after jump, and player can infinetly jump kirby style

--- tesy4900project/player/player.gd
+++ tesy4900project/player/player.gd
@@ -4,8 +4,10 @@ extends CharacterBody2D
 
 const GRAVITY = 1000
 const SPEED = 300
+const  JUMP_HEIGHT = -300
+const JUMP_HORIZONTAL = 100
 
-enum STATE { IDLE, RUN }
+enum STATE { IDLE, RUN, JUMP }
 
 var current_state
 
@@ -16,10 +18,13 @@ func _physics_process(delta):
 	player_falling(delta)
 	player_idle(delta)
 	player_run(delta)
+	player_jump(delta)
 	
 	move_and_slide()
 	
 	player_animations()
+	
+	print("State: ", STATE.keys()[current_state])
 
 func player_falling(delta):
 	if !is_on_floor():
@@ -28,23 +33,41 @@ func player_falling(delta):
 func player_idle(delta):
 	if is_on_floor():
 		current_state = STATE.IDLE
-		print("State: ", STATE.keys()[current_state])
+		#print("State: ", STATE.keys()[current_state])
 
 func player_run(delta):
+	if !is_on_floor():
+		return
+	
 	var direction = Input.get_axis("move_left", "move_right")
 	
 	if direction:
 		velocity.x = direction * SPEED
+		#animated_sprite_2d.flip_h = direction < 0
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
+	#if is_on_floor() and direction:
+		#current_state == STATE.RUN
+	
 	if direction != 0:
 		current_state = STATE.RUN
-		print("State: ", STATE.keys()[current_state])
+		#print("State: ", STATE.keys()[current_state])
 		animated_sprite_2d.flip_h = false if direction > 0 else true
+
+func player_jump(delta):
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_HEIGHT
+		current_state = STATE.JUMP
+		
+	if !is_on_floor() and current_state == STATE.JUMP:
+		var direction = Input.get_axis("move_left", "move_right")
+		velocity.x += direction * JUMP_HORIZONTAL * delta
 
 func player_animations():
 	if current_state == STATE.IDLE:
 		animated_sprite_2d.play("idle")
 	elif current_state == STATE.RUN:
 		animated_sprite_2d.play("run")
+	elif current_state == STATE.JUMP:
+		animated_sprite_2d.play("jump")
