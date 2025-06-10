839de63a4a30e630353492f0432d6f6feec80b97
David <DEmanuelCAnaya@gmail.com>
2025-06-10 17:02:28 -0600

    adjusted the player script to utilize delta time and made it better

--- tesy4900project/player/player.gd
+++ tesy4900project/player/player.gd
@@ -3,16 +3,12 @@ extends CharacterBody2D
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
 
 const GRAVITY = 1000
-@export var SPEED : int = 300
-@export var JUMP_HEIGHT : int = -325
-@export var JUMP_HORIZONTAL : int = 500
+@export var SPEED : int = 1000
+@export var MAX_HORIZONTAL_SPEED : int = 300
+@export var SLOW_DOWN_SPEED : int = 2000
+@export var JUMP_HEIGHT : int = -300
+@export var JUMP_HORIZONTAL_SPEED : int = 1000
 @export var JUMP_MAX_HORIZONTAL_SPEED : int = 300
-#const SPEED = 300
-#const  JUMP_HEIGHT = -325
-#const JUMP_HORIZONTAL = 500
-#const JUMP_MAX_HORIZONTAL_SPEED = 300
-
-
 
 enum STATE { IDLE, RUN, JUMP }
 
@@ -48,9 +44,10 @@ func player_run(delta : float):
 	var direction = input_movement()
 	
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x += direction * SPEED * delta
+		velocity.x = clamp(velocity.x, -MAX_HORIZONTAL_SPEED, MAX_HORIZONTAL_SPEED)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SLOW_DOWN_SPEED * delta)
 	
 	if direction != 0:
 		current_state = STATE.RUN
@@ -63,7 +60,7 @@ func player_jump(delta : float):
 		
 	if !is_on_floor() and current_state == STATE.JUMP:
 		var direction = input_movement()
-		velocity.x += direction * JUMP_HORIZONTAL * delta
+		velocity.x += direction * JUMP_HORIZONTAL_SPEED * delta
 		velocity.x = clamp(velocity.x, -JUMP_MAX_HORIZONTAL_SPEED, JUMP_MAX_HORIZONTAL_SPEED)
 
 func input_movement():
