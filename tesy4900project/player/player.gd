8918941e743921d6a6faf954f89a9a23cada0789
David <DEmanuelCAnaya@gmail.com>
2025-07-21 22:11:47 -0600

    fixed the rest of the player script. everything works now besides the multi-jump functionality. variables are exported so they can be further adjusted without needing to change the code. all that is left for the player is to add hurt, death, and attacks

--- tesy4900project/player/player.gd
+++ tesy4900project/player/player.gd
@@ -1,350 +1,109 @@
 extends CharacterBody2D
-
 class_name PlatformerController2D
 
-@export_category("Necesary Child Nodes")
-@export var PlayerSprite: AnimatedSprite2D
-@export var PlayerCollider: CollisionShape2D
+@export_category("Nodes")
+@export var sprite: AnimatedSprite2D
+@export var collider: CollisionShape2D
 
-@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-@onready var hand: Marker2D = $Hand
+@export_category("Horizontal Movement")
+@export_range(50.0, 600.0) var max_speed := 200.0
+@export_range(0.01, 3.0) var accel_time := 0.2
+@export_range(0.01, 3.0) var decel_time := 0.2
 
-#INFO HORIZONTAL MOVEMENT 
-@export_category("L/R Movement")
-##The max speed your player will move
-@export_range(50, 500) var maxSpeed: float = 200.0
-##How fast your player will reach max speed from rest (in seconds)
-@export_range(0, 4) var timeToReachMaxSpeed: float = 0.2
-##How fast your player will reach zero speed from max speed (in seconds)
-@export_range(0, 4) var timeToReachZeroSpeed: float = 0.2
-##If true, player will instantly move and switch directions. Overrides the "timeToReach" variables, setting them to 0.
-@export var directionalSnap: bool = false
-##If enabled, the default movement speed will by 1/2 of the maxSpeed and the player must hold a "run" button to accelerate to max speed. Assign "run" (case sensitive) in the project input settings.
-@export var runningModifier: bool = false
+@export_category("Jump & Gravity")
+@export_range(20.0, 500.0) var jump_height_px := 100.0
+@export_range(0.1, 1.0) var time_to_peak := 0.4
+@export_range(0.1, 1.0) var time_to_fall := 0.3
+@export_range(0.0, 0.5) var coyote_time := 0.2
+@export_range(0.0, 0.5) var jump_buffer_time := 0.2
+@export var max_jumps := 1
+@export var variable_jump := true
+@export_range(200.0, 2000.0) var terminal_velocity := 600.0
 
-#INFO JUMPING 
-@export_category("Jumping and Gravity")
-##The peak height of your player's jump
-@export_range(0, 20) var jumpHeight: float = 2.0
-##How many jumps your character can do before needing to touch the ground again. Giving more than 1 jump disables jump buffering and coyote time.
-@export_range(0, 4) var jumps: int = 1
-##The strength at which your character will be pulled to the ground.
-@export_range(0, 100) var gravityScale: float = 20.0
-##The fastest your player can fall
-@export_range(0, 1000) var terminalVelocity: float = 500.0
-##Your player will move this amount faster when falling providing a less floaty jump curve.
-@export_range(0.5, 3) var descendingGravityFactor: float = 1.3
-##Enabling this toggle makes it so that when the player releases the jump key while still ascending, their vertical velocity will cut in half, providing variable jump height.
-@export var shortHopAkaVariableJumpHeight: bool = true
-##How much extra time (in seconds) your player will be given to jump after falling off an edge. This is set to 0.2 seconds by default.
-@export_range(0, 0.5) var coyoteTime: float = 0.2
-##The window of time (in seconds) that your player can press the jump button before hitting the ground and still have their input registered as a jump. This is set to 0.2 seconds by default.
-@export_range(0, 0.5) var jumpBuffering: float = 0.2
-
-@export_category("Animations (Check Box if has animation)")
-##Animations must be named "run" all lowercase as the check box says
-@export var run: bool
-##Animations must be named "jump" all lowercase as the check box says
-@export var jump: bool
-##Animations must be named "idle" all lowercase as the check box says
-@export var idle: bool
-##Animations must be named "falling" all lowercase as the check box says
-@export var falling: bool
-
-#Variables determined by the developer set ones.
-var appliedGravity: float
-var maxSpeedLock: float
-var appliedTerminalVelocity: float
-
-var friction: float
-var acceleration: float
-var deceleration: float
-var instantAccel: bool = false
-var instantStop: bool = false
-
-var jumpMagnitude: float = 500.0
-var jumpCount: int
-var jumpWasPressed: bool = false
-var coyoteActive: bool = false
-var dashMagnitude: float
-var gravityActive: bool = true
-
-var twoWayDashHorizontal
-var twoWayDashVertical
-var eightWayDash
-
-var wasMovingR: bool
-var wasPressingR: bool
-var movementInputMonitoring: Vector2 = Vector2(true, true) #movementInputMonitoring.x addresses right direction while .y addresses left direction
-
-var gdelta: float = 1
-
-var dset = false
-
-var colliderScaleLockY
-var colliderPosLockY
-
-var anim
-var col
-var animScaleLock : Vector2
-
-#Input Variables for the whole script
-var downHold
-var leftHold
-var leftTap
-var leftRelease
-var rightHold
-var rightTap
-var rightRelease
-var jumpTap
-var jumpRelease
-
-var shuriken = preload("res://shuriken.tscn")
-var player_death_effect = preload("res://player/player_death_effect/player_death_effect.tscn")
+# Internal timers and physics values
+var coyote_timer := 0.0
+var jump_buffer_timer := 0.0
+var jumps_left := 0
+var jump_speed := 0.0
+var up_gravity := 0.0
+var down_gravity := 0.0
+var is_jumping := false
+var last_on_floor := false
 
 func _ready():
-	wasMovingR = true
-	anim = PlayerSprite
-	col = PlayerCollider
-	
-	_updateData()
-	
-func _updateData():
-	acceleration = maxSpeed / timeToReachMaxSpeed
-	deceleration = -maxSpeed / timeToReachZeroSpeed
-	
-	jumpMagnitude = (10.0 * jumpHeight) * gravityScale
-	jumpCount = jumps
-	
-	maxSpeedLock = maxSpeed
-	
-	animScaleLock = abs(anim.scale)
-	colliderScaleLockY = col.scale.y
-	colliderPosLockY = col.position.y
-	
-	if timeToReachMaxSpeed == 0:
-		instantAccel = true
-		timeToReachMaxSpeed = 1
-	elif timeToReachMaxSpeed < 0:
-		timeToReachMaxSpeed = abs(timeToReachMaxSpeed)
-		instantAccel = false
-	else:
-		instantAccel = false
-		
-	if timeToReachZeroSpeed == 0:
-		instantStop = true
-		timeToReachZeroSpeed = 1
-	elif timeToReachMaxSpeed < 0:
-		timeToReachMaxSpeed = abs(timeToReachMaxSpeed)
-		instantStop = false
-	else:
-		instantStop = false
-		
-	if jumps > 1:
-		jumpBuffering = 0
-		coyoteTime = 0
-	
-	coyoteTime = abs(coyoteTime)
-	jumpBuffering = abs(jumpBuffering)
-	
-	if directionalSnap:
-		instantAccel = true
-		instantStop = true
-	
-	twoWayDashHorizontal = false
-	twoWayDashVertical = false
-	eightWayDash = false	
-	
-
-func _process(_delta):
-	#INFO animations
-	# run
-	#if run and idle:
-		#if abs(velocity.x) > 0.1 and is_on_floor() and !is_on_wall():
-			#anim.speed_scale = abs(velocity.x / 150)
-			#if abs(velocity.x) < (maxSpeedLock):
-				#anim.play("walk")
-			#else:
-				#anim.play("run")
-		#elif abs(velocity.x) < 0.1 and is_on_floor():
-			#anim.speed_scale = 1
-			#anim.play("idle")
-	# jump
-	#if velocity.y < 0 and jump:
-		#anim.speed_scale = 1
-		#anim.play("jump")
-		
-	#if velocity.y > 40 and falling:
-		#anim.speed_scale = 1
-		#anim.play("falling")
-	
-	if !is_on_floor():
-		if velocity.y < 0:  # Jumping
-			anim.play("jump")
-		else:  # Falling
-			anim.play("falling")
-	else:
-		if abs(velocity.x) > 10:
-			anim.play("run")
-		else:
-			anim.play("idle")
+	jump_speed = -2.0 * jump_height_px / time_to_peak
+	up_gravity = 2.0 * jump_height_px / (time_to_peak * time_to_peak)
+	down_gravity = 2.0 * jump_height_px / (time_to_fall * time_to_fall)
+	jumps_left = max_jumps
 
 func _physics_process(delta):
-	if !dset:
-		gdelta = delta
-		dset = true
-	#INFO Input Detectio. Define your inputs from the project settings here.
-	leftHold = Input.is_action_pressed("left")
-	rightHold = Input.is_action_pressed("right")
-	downHold = Input.is_action_pressed("down")
-	leftTap = Input.is_action_just_pressed("left")
-	rightTap = Input.is_action_just_pressed("right")
-	leftRelease = Input.is_action_just_released("left")
-	rightRelease = Input.is_action_just_released("right")
-	jumpTap = Input.is_action_just_pressed("jump")
-	jumpRelease = Input.is_action_just_released("jump")
-	
-	#INFO Left and Right Movement
-	if rightHold and leftHold and movementInputMonitoring:
-		if !instantStop:
-			_decelerate(delta, false)
-		else:
-			velocity.x = -0.1
-	elif rightHold and movementInputMonitoring.x:
-		if velocity.x > maxSpeed or instantAccel:
-			velocity.x = maxSpeed
-		else:
-			velocity.x += acceleration * delta
-		if velocity.x < 0:
-			if !instantStop:
-				_decelerate(delta, false)
-			else:
-				velocity.x = -0.1
-	elif leftHold and movementInputMonitoring.y:
-		if velocity.x < -maxSpeed or instantAccel:
-			velocity.x = -maxSpeed
-		else:
-			velocity.x -= acceleration * delta
-		if velocity.x > 0:
-			if !instantStop:
-				_decelerate(delta, false)
-			else:
-				velocity.x = 0.1
-				
-	if velocity.x > 0:
-		wasMovingR = true
-	elif velocity.x < 0:
-		wasMovingR = false
-		
-	if rightTap:
-		wasPressingR = true
-	if leftTap:
-		wasPressingR = false
-	
-	if !(leftHold or rightHold):
-		if !instantStop:
-			_decelerate(delta, false)
-		else:
-			velocity.x = 0
-			
-	#INFO Jump and Gravity
-	if velocity.y > 0:
-		appliedGravity = gravityScale * descendingGravityFactor
+	# timers
+	if is_on_floor():
+		coyote_timer = coyote_time
+		jumps_left = max_jumps
+		if not is_jumping:
+			handle_grounded_animations()
 	else:
-		appliedGravity = gravityScale
-	
-	if gravityActive:
-		if velocity.y < appliedTerminalVelocity:
-			velocity.y += appliedGravity
-		elif velocity.y > appliedTerminalVelocity:
-				velocity.y = appliedTerminalVelocity
-		
-	if shortHopAkaVariableJumpHeight and jumpRelease and velocity.y < 0:
-		velocity.y = velocity.y / 2
-	
-	if jumps == 1:
-		if !is_on_floor() and !is_on_wall():
-			if coyoteTime > 0:
-				coyoteActive = true
-				_coyoteTime()
-				
-		if jumpTap and !is_on_wall():
-			if coyoteActive:
-				coyoteActive = false
-				_jump()
-			if jumpBuffering > 0:
-				jumpWasPressed = true
-				_bufferJump()
-			elif jumpBuffering == 0 and coyoteTime == 0 and is_on_floor():
-				_jump()	
-		elif jumpTap and is_on_floor():
-			_jump()
-			
-		if is_on_floor():
-			jumpCount = jumps
-			coyoteActive = true
-			if jumpWasPressed:
-				_jump()
-				
-	elif jumps > 1:
-		if is_on_floor():
-			jumpCount = jumps
-		elif jumpTap and jumpCount > 0:
-			velocity.y = -jumpMagnitude
-			jumpCount = jumpCount - 1
+		coyote_timer = max(coyote_timer - delta, 0.0)
+		handle_airborne_animations()
+
+	# gravity
+	apply_gravity(delta)
+
+	# horizontal
+	handle_horizontal_movement(delta)
+
+	# jumping
+	handle_jumping(delta)
+
 	move_and_slide()
 
-func _bufferJump():
-	await get_tree().create_timer(jumpBuffering).timeout
-	jumpWasPressed = false
+func handle_grounded_animations():
+	var dir = Input.get_action_strength("right") - Input.get_action_strength("left")
+	if abs(dir) > 0.1:
+		sprite.speed_scale = abs(velocity.x) / max_speed
+		sprite.play("run")
+	else:
+		sprite.play("idle")
+	sprite.flip_h = velocity.x < 0.0
 
-func _coyoteTime():
-	await get_tree().create_timer(coyoteTime).timeout
-	coyoteActive = false
-	jumpCount += -1
+func handle_airborne_animations():
+	if velocity.y < 0.0:
+		sprite.play("jump")
+	elif velocity.y > 0.0:
+		sprite.play("falling")
+	if not is_on_floor() and not is_jumping and Input.is_action_pressed("down"):
+		sprite.play("falling")
 
-func _jump():
-	if jumpCount > 0:
-		velocity.y = -jumpMagnitude
-		jumpCount += -1
-		jumpWasPressed = false
-		
+func apply_gravity(delta):
+	if not is_on_floor():
+		var g = up_gravity if velocity.y < 0.0 else down_gravity
+		velocity.y = min(velocity.y + g * delta, terminal_velocity)
 
-func _inputPauseReset(time):
-	await get_tree().create_timer(time).timeout
-	movementInputMonitoring = Vector2(true, true)
-	
+func handle_horizontal_movement(delta):
+	var dir = Input.get_action_strength("right") - Input.get_action_strength("left")
+	var target_speed = dir * max_speed
+	var accel = abs(target_speed - velocity.x) / accel_time
+	velocity.x = move_toward(velocity.x, target_speed, accel * delta)
 
-func _decelerate(delta, vertical):
-	if !vertical:
-		if velocity.x > 0:
-			velocity.x += deceleration * delta
-		elif velocity.x < 0:
-			velocity.x -= deceleration * delta
-	elif vertical and velocity.y > 0:
-		velocity.y += deceleration * delta
+func handle_jumping(delta):
+	# buffer jump
+	if Input.is_action_just_pressed("jump"):
+		jump_buffer_timer = jump_buffer_time
+	else:
+		jump_buffer_timer = max(jump_buffer_timer - delta, 0.0)
 
+	# perform jump
+	if jump_buffer_timer > 0.0 and coyote_timer > 0.0 and jumps_left > 0:
+		velocity.y = jump_speed
+		jumps_left -= 1
+		jump_buffer_timer = 0.0
+		is_jumping = true
 
-func _pauseGravity(time):
-	gravityActive = false
-	await get_tree().create_timer(time).timeout
-	gravityActive = true
+	# variable jump on release
+	if variable_jump and Input.is_action_just_released("jump") and velocity.y < 0.0:
+		velocity.y *= 0.5
 
-func player_death():
-	var player_death_effect_instance = player_death_effect.instantiate() as Node2D
-	player_death_effect_instance.global_position = global_position
-	get_parent().add_child(player_death_effect_instance)
-	queue_free()
-
-func _on_hurtbox_body_entered(body: Node2D) -> void:
-	if body.is_in_group("Enemy"):
-		print("Enemy Entered ", body.damage_amount)
-		
-		var tween = get_tree().create_tween()
-		tween.tween_property(animated_sprite_2d, "material:shader_parammeter/enabled", true, 0)
-		tween.tween_property(animated_sprite_2d, "material:shader_parammeter/enabled", false, 0.2)
-		
-		HealthManager.decrease_health(body.damage_amount)
-		
-		if HealthManager.current_health == 0:
-			player_death()
+	# reset jump state when landing
+	if is_jumping and is_on_floor():
+		is_jumping = false
